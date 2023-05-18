1eb0a8b1e74c2d4b6e73e6b8ca234291fb95e13b
Dudi1896 <denzelu181@gmail.com>
2023-05-18 13:36:50 -0400

    modifed to use Excel sheet

--- LetterUpdater.py
+++ LetterUpdater.py
@@ -1,80 +1,83 @@
 """Author: Denzel Udemba
 Date: 05/10/2023
-Description: This code uses a basic word letter template addressed
-to GA State representatives and adds variables in place of their names
-districts, Rooms, and cities they represent. Data is sourced from a private .env file"""
+Description: This code uses a basic word letter template and adds
+data points from an Excel spreadsheet in order to make multiple
+versions of the template file"""
 
 import os
 import shutil
 from docx import Document
 from dotenv import load_dotenv
 from docx2pdf import convert
+from openpyxl import load_workbook
 
 
 def main():
-    # Load .env variables
     load_dotenv()
 
-    # Get paths from .env variables
-    source_file = os.getenv("SOURCE_FILE")
-    target_directory = os.getenv("TARGET_DIRECTORY")
-    pdf_directory = os.getenv("PDF_DIRECTORY")
-    target_files = os.getenv("TARGET_FILE").split(",")
-    date = os.getenv("__DATE__")
-    senator_names = os.getenv("__SENATOR_NAME__").split(",")
-    senator_last_names = os.getenv("__SEN_LNAME__").split(",")
-    district_nos = os.getenv("__DISTRICT_NO__").split(",")
-    room_nos = os.getenv("__ROOM_NO__").split(",")
-    city_variables = os.getenv("__CITY__").split(",")
+    # Get paths from .env file
+    source_file = os.getenv("WORD_TEMPLATE_SOURCE_FILE")
+    word_files_created = os.getenv("WORD_FILES_CREATED")
+    pdf_files_created = os.getenv("PDF_FILES_CREATED")
+    excel_file = os.getenv("XL_FILE_LOCATION")
+    worksheet_name = os.getenv("WORKSHEET_NAME")
 
-    # Loop through each target file
-    for new_file, senator_name, senator_last_name, district_no, room_no, \
-        city_variable in zip(target_files,
-                             senator_names, senator_last_names, district_nos, room_nos, city_variables):
-        # Copy source file to target directory
-        shutil.copyfile(source_file, os.path.join(target_directory, new_file))
+    # Load excel_dict_variables from Excel file
+    excel_workbook = load_workbook(filename=excel_file, read_only=False)
+    work_sheet = excel_workbook[worksheet_name]
+    excel_dict_variables = {}
 
-        # Open target file for editing
-        document = Document(os.path.join(target_directory, new_file))
+    for row in work_sheet.iter_rows(min_row=2, values_only=True):
+        excel_key = row[0]
+        excel_values = row[1:]
+        excel_dict_variables[excel_key] = excel_values
+        business_name = excel_dict_variables[excel_key][2]
+        title = excel_dict_variables[excel_key][3]
+        date = excel_dict_variables[excel_key][0]
+        date_str = date.strftime('%B %d, %Y')
+        date_for_title = date.strftime('%B-%d-%Y')
+        contractor_name = excel_dict_variables[excel_key][1]
+        target_files = [f"LightUp_ATL_GA_{file.strip()}_{date_for_title}.docx"
+                        for file in excel_dict_variables[excel_key][1].split(',')]
 
-        # parse through new_file & change the following field if applicable
-        for paragraph in document.paragraphs:
-            if "__DATE__" in paragraph.text:
-                paragraph.text = paragraph.text.replace("__DATE__", date)
-                print(f"Date: {date} changed successfully")
+        for new_file in target_files:
+            # Copy source file to new word files directory
+            shutil.copyfile(source_file, os.path.join(word_files_created, new_file))
 
-        for paragraph in document.paragraphs:
-            if "__SENATOR_NAME__" in paragraph.text:
-                paragraph.text = paragraph.text.replace("__SENATOR_NAME__", senator_name)
-                print(f"Senator Name: {senator_name} changed successfully")
+            # Open new file for editing
+            document = Document(os.path.join(word_files_created, new_file))
 
-        for paragraph in document.paragraphs:
-            if "__SEN_LNAME__" in paragraph.text:
-                paragraph.text = paragraph.text.replace("__SEN_LNAME__", senator_last_name)
-                print(f"Senator Lastname: {senator_last_name} changed successfully")
+            # Parse through new_file & change the following field if applicable
+            for paragraph in document.paragraphs:
+                if "__DATE__" in paragraph.text:
+                    paragraph.text = paragraph.text.replace("__DATE__", str(date_str))
+                    print(f"Date: {date} changed successfully")
 
-        for paragraph in document.paragraphs:
-            if "__DISTRICT_NO__" in paragraph.text:
-                paragraph.text = paragraph.text.replace("__DISTRICT_NO__", district_no)
-                print(f"{district_no} changed successfully")
+            for paragraph in document.paragraphs:
+                if "__NAME__" in paragraph.text:
+                    paragraph.text = paragraph.text.replace("__NAME__", contractor_name)
+                    print(f"Contractor Name: {contractor_name} changed successfully")
 
-        for paragraph in document.paragraphs:
-            if "__ROOM_NO__" in paragraph.text:
-                paragraph.text = paragraph.text.replace("__ROOM_NO__", room_no)
-                print(f"{room_no} changed successfully")
+            for paragraph in document.paragraphs:
+                if "__NAME_OF_BUSINESS__" in paragraph.text:
+                    paragraph.text = paragraph.text.replace("__NAME_OF_BUSINESS__", business_name)
+                    print(f"Business Name: {business_name} changed successfully")
 
-        for paragraph in document.paragraphs:
-            if "__CITY__" in paragraph.text:
-                paragraph.text = paragraph.text.replace("__CITY__", city_variable)
-                print(f"City Name: {city_variable} changed successfully")
+            for paragraph in document.paragraphs:
+                if "__TITLE__" in paragraph.text:
+                    paragraph.text = paragraph.text.replace("__TITLE__", title)
+                    print(f"Position: {title} changed successfully")
 
-        # Save updated file
-        document.save(os.path.join(target_directory, f"{new_file}"))
-        print(f"{new_file}: saved successfully to {os.path.join(target_directory, f'{new_file}')}")
+            # Save updated file
+            document.save(os.path.join(word_files_created, f"{new_file}"))
+            print(f"{new_file}: saved successfully to {os.path.join(word_files_created, f'{new_file}')}")
 
-        # Convert the file to PDF
-        convert(os.path.join(target_directory, new_file), os.path.join(pdf_directory, f"{new_file}.pdf"))
-        print(f"{new_file}: converted successfully to {os.path.join(pdf_directory, f'{new_file}.pdf')}")
+            # Convert the file to PDF
+            convert(os.path.join(word_files_created, new_file),
+                    os.path.join(pdf_files_created, f"{new_file}.pdf"))
+
+            print(f"{new_file}: converted successfully to"
+                  f" {os.path.join(pdf_files_created, f'{new_file}.pdf')}")
 
 
 if __name__ == '__main__':
